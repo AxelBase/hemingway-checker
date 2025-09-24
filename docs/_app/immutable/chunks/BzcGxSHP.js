99c1b83975cd81df4f05e0f9e0fd527e0915fd5c
Sahil Ramjeeth <22046468@dut4life.ac.za>
2025-09-24 03:09:24 +0200

    Update docs/ with latest build including GSC verification file

--- docs/_app/immutable/chunks/BzcGxSHP.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_yzidv4?.base??"/hemingway-checker",e=globalThis.__sveltekit_yzidv4?.assets??s;export{e as a,s as b};
---
+++ docs/_app/immutable/chunks/DwApIiqx.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_t5cd2t?.base??"/hemingway-checker",t=globalThis.__sveltekit_t5cd2t?.assets??s;export{t as a,s as b};
