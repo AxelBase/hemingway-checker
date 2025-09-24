813a96462d555a6a45251e129cb7f8e85a7d178b
Sahil Ramjeeth <22046468@dut4life.ac.za>
2025-09-24 02:02:24 +0200

    Add GSC verification tag and update docs/

--- docs/_app/immutable/chunks/BIILvm9d.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_zimyft?.base??"/hemingway-checker",e=globalThis.__sveltekit_zimyft?.assets??s;export{e as a,s as b};
---
+++ docs/_app/immutable/chunks/ypi9OXTE.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_17mihjs?.base??"/hemingway-checker",e=globalThis.__sveltekit_17mihjs?.assets??s;export{e as a,s as b};
