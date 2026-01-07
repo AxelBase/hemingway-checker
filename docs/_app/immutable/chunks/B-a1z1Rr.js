87af273fed9dfe9defede593959ab29d020b3911
Sahil Ramjeeth <22046468@dut4life.ac.za>
2026-01-07 20:10:24 +0200

    Fix B

--- docs/_app/immutable/chunks/B-a1z1Rr.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1yrcbyq?.base??"/hemingway-checker",e=globalThis.__sveltekit_1yrcbyq?.assets??s;export{e as a,s as b};
---
+++ docs/_app/immutable/chunks/CXoex8Ax.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1dotk9m?.base??"/hemingway-checker",e=globalThis.__sveltekit_1dotk9m?.assets??s;export{e as a,s as b};
